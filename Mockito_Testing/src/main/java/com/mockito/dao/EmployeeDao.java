eabbfa669437becfa15f83fdbb2310dde4bc3ec5
naman kumar singh <singhnaman320@gmail.com>
2023-04-29 18:35:29 +0530

    Addition of Dao layer

--- Mockito_Testing/src/main/java/com/mockito/dao/EmployeeDao.java
+++ Mockito_Testing/src/main/java/com/mockito/dao/EmployeeDao.java
@@ -1,4 +1,14 @@
 package com.mockito.dao;
 
-public class EmployeeDao {
+import com.mockito.exceptions.NoRecordFoundException;
+import com.mockito.exceptions.SomeThingWrongException;
+import com.mockito.modelDTO.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+
+    public void addEmployee(Employee employee) throws SomeThingWrongException;
+
+    public List<Employee> getEmployeeList() throws NoRecordFoundException;
 }
