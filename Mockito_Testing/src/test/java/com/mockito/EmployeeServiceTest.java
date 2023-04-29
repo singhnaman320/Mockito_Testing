f43c28e3081dd3877016d2716ee00c4742e9103d
naman kumar singh <singhnaman320@gmail.com>
2023-04-29 20:09:22 +0530

    Test Addition for add employee

--- Mockito_Testing/src/test/java/com/mockito/EmployeeServiceTest.java
+++ Mockito_Testing/src/test/java/com/mockito/EmployeeServiceTest.java
@@ -1,4 +1,21 @@
 package com.mockito;
 
+import com.mockito.dao.EmployeeDao;
+import com.mockito.services.EmployeeService;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
 public class EmployeeServiceTest {
+
+    private EmployeeService employeeService;
+
+    private EmployeeDao createMockForInsertion(){
+
+        //creating mock object using mockito
+
+        EmployeeDao employeeDao= mock(EmployeeDao.class);
+
+
+    }
 }
