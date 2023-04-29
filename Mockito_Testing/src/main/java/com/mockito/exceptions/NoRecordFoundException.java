6f58b90ef4e6e739f2ac826f9336cf2bb8b954e4
naman kumar singh <singhnaman320@gmail.com>
2023-04-29 17:55:37 +0530

    Addition of DTO

---
+++ Mockito_Testing/src/main/java/com/mockito/exceptions/NoRecordFoundException.java
@@ -0,0 +1,12 @@
+package com.mockito.exceptions;
+
+public class NoRecordFoundException extends Exception{
+
+    public NoRecordFoundException(){
+
+    }
+    public NoRecordFoundException(String message){
+
+        super(message);
+    }
+}
