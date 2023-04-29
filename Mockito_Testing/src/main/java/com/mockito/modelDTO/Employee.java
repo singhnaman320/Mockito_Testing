3998667742e616d35f309ddec0df912f97e1a86d
naman kumar singh <singhnaman320@gmail.com>
2023-04-29 17:14:42 +0530

    Addition of DTO

--- Mockito_Testing/src/main/java/com/mockito/modelDTO/Employee.java
+++ Mockito_Testing/src/main/java/com/mockito/modelDTO/Employee.java
@@ -10,4 +10,42 @@ public class Employee {
     public Employee() {
     }
 
+    public Employee(Integer empId, String name, String salary, String department) {
+        this.empId = empId;
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public Integer getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
