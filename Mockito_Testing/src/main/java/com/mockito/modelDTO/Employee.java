e1d07a6c5da2d38812c1a7b531ef6f6ee3736527
naman kumar singh <singhnaman320@gmail.com>
2023-04-29 17:13:55 +0530

    Addition of DTO

--- Mockito_Testing/src/main/java/com/mockito/modelDTO/Employee.java
+++ Mockito_Testing/src/main/java/com/mockito/modelDTO/Employee.java
@@ -1,4 +1,13 @@
 package com.mockito.modelDTO;
 
 public class Employee {
+
+    private Integer empId;
+    private String name;
+    private String salary;
+    private String department;
+
+    public Employee() {
+    }
+
 }
