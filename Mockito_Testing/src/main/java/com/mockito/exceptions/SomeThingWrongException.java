6a8d5d8731274615d3e57b8de29d38e04716c12f
naman kumar singh <singhnaman320@gmail.com>
2023-04-29 17:59:47 +0530

    Addition of Exceptions

---
+++ Mockito_Testing/src/main/java/com/mockito/exceptions/SomeThingWrongException.java
@@ -0,0 +1,12 @@
+package com.mockito.exceptions;
+
+public class SomeThingWrongException extends Exception{
+
+    public SomeThingWrongException(){
+
+    }
+    public SomeThingWrongException(String message){
+
+        super(message);
+    }
+}
